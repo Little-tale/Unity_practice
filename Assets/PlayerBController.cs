b5ba3863105dcc34b80a7ed8477322f5b2933c2e
KimJaeHyung <you3192001@icloud.com>
2025-11-05 16:58:21 +0900

    [Feat] first my unity

---
+++ Assets/PlayerBController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerBController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello My First Unity from Player B Controller");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("WOOOW");
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hello My First Unity");
+        // fps 고정 세팅
+        Application.targetFrameRate = 144;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
